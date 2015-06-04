aee5a8ccc9d1fd11fabe79f34bc567b477bd3f6a
Gareth Rees <gareth@mysociety.org>
2015-06-04 11:31:55 +0100

    Explicitly define each callback
    Just not worth using an Array. Prevents easily adding conditionals and
    reordering.

--- app/models/public_body.rb
+++ app/models/public_body.rb
@@ -68,9 +68,9 @@ class PublicBody < ActiveRecord::Base
 
     validate :request_email_if_requestable
 
-    before_save :set_api_key,
-                :set_default_publication_scheme,
-                :set_first_letter
+    before_save :set_api_key
+    before_save :set_default_publication_scheme
+    before_save :set_first_letter
     after_save :purge_in_cache
     after_update :reindex_requested_from
 
